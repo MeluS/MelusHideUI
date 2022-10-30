ffb7d598c37c417a19825753d7ef628e3d8e61cf
MeluS <39301703+MeluS@users.noreply.github.com>
2022-10-30 18:37:17 +0100

    Update MelusHideUI.lua

--- MelusHideUI.lua
+++ MelusHideUI.lua
@@ -9,32 +9,6 @@ local function HideUICustom()
     TargetFrame.TargetFrameContainer.FrameTexture:Hide()
  end
 
-local MelusHideUI = CreateFrame("Frame", "MuteGroupSounds")
-MicroButtonAndBagsBar:RegisterForDrag("LeftButton")
-MicroButtonAndBagsBar:SetScript("OnDragStart", MicroButtonAndBagsBar.StartMoving)
-MicroButtonAndBagsBar:SetScript("OnDragStop", MicroButtonAndBagsBar.StopMovingOrSizing)
-
-MicroButtonAndBagsBar:SetMovable(true)
-MicroButtonAndBagsBar:EnableMouse(true)
-MicroButtonAndBagsBar:SetScript("OnMouseDown", function(self, button)
-  if button == "LeftButton" and not self.isMoving then
-   self:StartMoving();
-   self.isMoving = true;
-  end
-end)
-MicroButtonAndBagsBar:SetScript("OnMouseUp", function(self, button)
-  if button == "LeftButton" and self.isMoving then
-   self:StopMovingOrSizing();
-   self.isMoving = false;
-  end
-end)
-MicroButtonAndBagsBar:SetScript("OnHide", function(self)
-  if ( self.isMoving ) then
-   self:StopMovingOrSizing();
-   self.isMoving = false;
-  end
-end)
-
 MelusHideUI:RegisterEvent("PLAYER_ENTERING_WORLD")
 MelusHideUI:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 MelusHideUI:SetScript("OnEvent",
